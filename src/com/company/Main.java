476f3c2137da475b0e717a7dedb4550fb140adb6
wigglewie <timoxa717@gmail.com>
2021-06-02 00:14:58 +0300

    finished double transposition decoder

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,68 +4,71 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        String sentence = "ТООГ_СШЕЕЮТАПЛРИ";
-        var test = "test";
-        String sentence = "_СШЕ";
+        String sentence = "ТООГ_СШЕЕЮТАПЛРИ";
         int x = 4;
-        int y = 1;
+        int y = 4;
         int xSequenceValue = 1342;
         int ySequenceValue = 4321;
         System.out.println(task1(sentence, x, y, xSequenceValue, ySequenceValue));
 
-
     }
 
     private static String task1(String sentence, int x, int y, int xSequenceValue, int ySequenceValue) {
-        var result = "";
+        StringBuilder result = new StringBuilder();
 
         char[] sentenceAsCharArray = sentence.toCharArray();
 
         char[][] matrix = new char[y][x];
+        char[][] matrixProperX = new char[y][x];
+        char[][] matrixProperY = new char[y][x];
 
+        //getting x sequence
         int[] xSequence = new int[x];
         for (int j = xSequence.length - 1; j >= 0; j--) {
             xSequence[j] = xSequenceValue % 10;
             xSequenceValue /= 10;
         }
 
+        //getting y sequence
         int[] ySequence = new int[x];
         for (int j = ySequence.length - 1; j >= 0; j--) {
             ySequence[j] = ySequenceValue % 10;
             ySequenceValue /= 10;
         }
 
-
-        int step = 0;
-
         if (sentenceAsCharArray.length < x * y && sentenceAsCharArray.length > x * y) {
             return "error";
         }
 
+        //getting matrix filled with values
+        int step = 0;
         for (int j = 0; j < y; j++) {
-
             for (int z = 0; z < x; z++) {
                 matrix[j][z] = sentenceAsCharArray[step];
                 step++;
             }
-
         }
 
-        System.out.println();
-
-        char[][] matrixCopy = matrix;
-
+        //filtering X
         for (int j = 0; j < y; j++) {
             for (int z = 0; z < x; z++) {
-                char temp = matrix[j][xSequence[z] - 1];
-                matrix[j][xSequence[z] - 1] = matrix[j][z];
-                matrix[j][z] = temp;
-
-                System.out.println();
+                matrixProperX[j][z] = matrix[j][xSequence[z] - 1];
             }
         }
 
+        //filtering Y
+        for (int j = 0; j < y; j++) {
+            for (int z = 0; z < x; z++) {
+                matrixProperY[j][z] = matrixProperX[ySequence[j] - 1][z];
+            }
+        }
 
-        return result;
+        //getting result
+        for (int j = 0; j < y; j++) {
+            for (int z = 0; z < x; z++) {
+                result.append(matrixProperY[j][z]);
+            }
+        }
+        return result.toString();
     }
 }
